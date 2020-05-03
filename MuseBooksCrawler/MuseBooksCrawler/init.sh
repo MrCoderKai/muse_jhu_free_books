28f947d8a4d182cb427fe22836412f9fdeae7e40
chengkai <chengkaiupc@163.com>
2020-05-03 14:03:53 +0800

    update init.sh

--- MuseBooksCrawler/MuseBooksCrawler/init.sh
+++ MuseBooksCrawler/MuseBooksCrawler/init.sh
@@ -1,3 +1,4 @@
+pip install xlrd scrapy
 mkdir -p /root/book/src
 mkdir -p /root/book/crawl_complete
 mkdir -p /root/book/failed
@@ -6,5 +7,5 @@ mkdir -p /root/book/downloading
 mkdir -p /root/book/pdf
 echo 60 > /root/book/book_sleep_high
 echo 30 > /root/book/book_sleep_low
-echo 40 > /root/book/chapter_sleep_high
-echo 20 > /root/book/chapter_sleep_low
+echo 20 > /root/book/chapter_sleep_high
+echo 10 > /root/book/chapter_sleep_low
